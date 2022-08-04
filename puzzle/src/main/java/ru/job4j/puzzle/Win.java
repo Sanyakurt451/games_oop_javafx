028e46fd207c111a47a1884fe0c7b7950445b631
sanya <batalovalexande@gmail.com>
2022-08-04 22:10:32 +0500

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -23,12 +23,12 @@ public class Win {
             return result;
         }
 
-                public static boolean check(int[][] board) {
-                    boolean rsl = false;
-                    for (int i = 0; i < board.length; i++) {
-                        if (board[i][i] == 1 && (monoHorizontal(board, i) || monoVertical(board, i))) {
-                           rsl = true;
-                            break;
+        public static boolean check(int[][] board) {
+             boolean rsl = false;
+             for (int i = 0; i < board.length; i++) {
+                 if (board[i][i] == 1 && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                     rsl = true;
+                     break;
                         }
                     }
                     return rsl;
